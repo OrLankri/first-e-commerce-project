ea3a0331127ef81ac97ac14fd8f51f2c2c00437a
Or Lankri <lankri469@gmail.com>
2022-01-24 15:58:30 +0200

    creating link

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,7 +49,7 @@ class Directory extends React.Component{
     render(){
       return(
         <div className='directory-menu'>
-          {this.state.sections.map(({title,imageUrl,id,size})=> (<MenuItem key={id} imageUrl={imageUrl} title={title.toUpperCase()} size={size}/>))}
+          {this.state.sections.map(({id, ...otherSectionProps})=> (<MenuItem key={id} {...otherSectionProps}/>))}
 
         </div>
       )
